3df8ac78b08a59630a1c936981009982968503da
Carlos Rueda <carueda@mbari.org>
2016-01-14 18:09:32 -0800

    comment

--- src/app/js/config.js
+++ src/app/js/config.js
@@ -1,11 +1,14 @@
 var appConfig = {
   help: "https://github.com/mmisw/orr-portal",
-  orront: {
-    rest: "https://mmisw.org/orr-ont",
-    selfHostPrefix: "http://mmisw.org/ont/",
-    secret: "",
-    sigParamName: "sgn",
 
+  orront: {
+    // orr-ont endpoint (without trailing slash)
+    rest: "https://mmisw.org/orr-ont",
+
+    // prefix to recognize "self-resolvable" URLs
+    selfHostPrefix: "http://mmisw.org/ont/",
+
+    // SPARQL endpoint
     sparqlEndpoint: 'https://mmisw.org/sparql'
   }
 
