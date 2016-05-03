01295c82670ebe151f25e1234c51f2495745f6d3
Carlos Rueda <carueda@gmail.com>
2016-05-02 22:22:43 -0700

    add vocabulary.js with some defs

---
+++ src/app/js/vocabulary/vocabulary.js
@@ -0,0 +1,24 @@
+var vocabulary = (function() {
+  'use strict';
+
+  return {
+    omv: {
+      hasCreator: {
+        uri: "http://omv.ontoware.org/2005/05/ontology#hasCreator",
+        label: "Ontology Creator"
+      },
+      keywords: {
+        uri: "http://omv.ontoware.org/2005/05/ontology#keywords",
+        label: "Keywords"
+      }
+    },
+
+    omvmmi: {
+      origMaintainerCode: {
+        uri: "http://mmisw.org/ont/mmi/20081020/ontologyMetadata/origMaintainerCode",
+        label: "Organization"
+      }
+    }
+  }
+
+})();
