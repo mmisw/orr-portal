bcb7eec3590e06a750c4f9f8033e2749c8336788
Carlos Rueda <carueda@gmail.com>
2016-07-05 18:49:49 -0700

    put alpha testing tooltip in a temporary directive <orr-testing>

--- src/app/js/main/main.js
+++ src/app/js/main/main.js
@@ -6,6 +6,9 @@
 
   angular.module('orrportal.main', ['orrportal.directives', 'orrportal.filters', 'orrportal.services'])
     .directive('orrMain',  MainDirective)
+
+    // TODO remove after alpha testing
+    .directive('orrTesting', function() { return { restrict: 'E', templateUrl: 'js/main/testing.html' } })
   ;
 
   MainDirective.$inject = [];
